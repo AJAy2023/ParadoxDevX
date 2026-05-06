10175b29b8684ad55d33217d46367a47c52f6a9b
Ajay <ajayjadhav0913@gmail.com>
2026-05-07 00:32:32 +0530

    done with  og-image and small change in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
               <a href="https://x.com/ParadoxDevX" target="_blank" className="border border-[#333] p-3 text-[#888] hover:bg-[#111] hover:text-white hover:border-[#555] transition-all rounded-sm flex items-center justify-center">
                 <svg viewBox="0 0 24 24" aria-hidden="true" className="w-[18px] h-[18px] fill-current"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></svg>
               </a>
-              <a href="https://github.com/AJAy2023/" target="_blank" className="border border-[#333] p-3 text-[#888] hover:bg-[#111] hover:text-white hover:border-[#555] transition-all rounded-sm flex items-center justify-center">
+              <a href="https://github.com/Paradoxdevx" target="_blank" className="border border-[#333] p-3 text-[#888] hover:bg-[#111] hover:text-white hover:border-[#555] transition-all rounded-sm flex items-center justify-center">
                 <Github size={18}/>
               </a>
               <a href="https://www.linkedin.com/company/paradoxdevx/" target="_blank" className="border border-[#333] p-3 text-[#888] hover:bg-[#111] hover:text-white hover:border-[#555] transition-all rounded-sm flex items-center justify-center">
